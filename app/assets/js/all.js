9a3b09e0451e1b99f1bf16c8bfe75ed62c9ccdaa
Yiren-Liou <liou830129@gmail.com>
2020-06-08 00:52:04 +0800

    Initial commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  // 手機menu
+  $(".smMenuOpen").click(function () {
+    $(".smMenu").toggleClass("move");
+  });
+
+  $(".smMenuClose").click(function () {
+    $(".smMenu").toggleClass("move");
+  });
+  // 下拉選單icon
+  $(".dropdown").on("click", function (event) {
+    event.preventDefault();
+    $(".dropdown-btn").toggleClass("active");
+  });
+  //購物車列表
+  $(".shopCartOpen").click(function () {
+    $(".shopCart").toggleClass("move");
+  });
+  $(".shopCartClose").click(function () {
+    $(".shopCart").toggleClass("move");
+  });
+ 
+});
