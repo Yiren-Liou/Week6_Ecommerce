7d6bcc64b32d742f9e290ed749ae6f25df353a47
Yiren-Liou <liou830129@gmail.com>
2020-06-08 00:59:00 +0800

    Update 2020-06-07T16:58:44.932Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,23 +1,24 @@
+"use strict";
+
 $(document).ready(function () {
   // 手機menu
   $(".smMenuOpen").click(function () {
     $(".smMenu").toggleClass("move");
   });
-
   $(".smMenuClose").click(function () {
     $(".smMenu").toggleClass("move");
-  });
-  // 下拉選單icon
+  }); // 下拉選單icon
+
   $(".dropdown").on("click", function (event) {
     event.preventDefault();
     $(".dropdown-btn").toggleClass("active");
-  });
-  //購物車列表
+  }); //購物車列表
+
   $(".shopCartOpen").click(function () {
     $(".shopCart").toggleClass("move");
   });
   $(".shopCartClose").click(function () {
     $(".shopCart").toggleClass("move");
   });
- 
 });
+//# sourceMappingURL=all.js.map
